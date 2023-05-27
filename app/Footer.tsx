6c6ebcb844d0b0f4738d70072fe332f326749db0
cocdeshijie <2865509822ma@gmail.com>
2023-05-28 00:03:11 +0800

    add: footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className={"border-t bg-white/60 backdrop-blur-md py-8"}>
+            <div className={"mx-auto flex items-center justify-between px-8"}>
+                <div className="mt-8 md:order-1 md:mt-0">
+                    <p className="text-center text-xs leading-5 text-gray-500">
+                        &copy; 2020 Your Company, Inc. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
     )
 }
