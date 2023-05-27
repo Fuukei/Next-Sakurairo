1dd4ac1d97921e61581fbd6c1179b14c6c979e9b
cocdeshijie <2865509822ma@gmail.com>
2023-05-28 00:04:28 +0800

    add: basic landing page info structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
-import Image from 'next/image'
 import ArticleCard from "@/app/ArticleCard";
 
 export default function Home() {
   return (
       <>
-          <div className={"grid flex-1"}>
-              <div className={"flex items-center justify-center min-h-screen bg-blue-100"}>
-                  landing screen
-              </div>
-              <main className={"bg-green-100"}>
-                  <div className={"mx-4 lg:mx-auto lg:w-[850px]"}>
-                      <h1 className={"font-heading text-xl md:text-2 xl my-6"}> Articles</h1>
-
-                      <ArticleCard />
-                      <ArticleCard />
+          <div className={"grid content-center justify-items-center min-h-screen"}>
+              <div className={"w-10/12 max-w-lg"}>
+                  <div className={"bg-green-100/75 backdrop-blur-sm w-full text-center rounded-xl py-6"}>
+                      <span>誰でも大好き!</span>
                   </div>
-              </main>
+              </div>
           </div>
+
+          <main className={"bg-white/60 backdrop-blur-md"}>
+              <div className={"mx-4 lg:mx-auto lg:w-[850px]"}>
+                  <h1 className={"font-heading text-xl md:text-2 xl my-6"}> Articles</h1>
+
+                  <ArticleCard />
+                  <ArticleCard />
+              </div>
+          </main>
       </>
   )
 }
