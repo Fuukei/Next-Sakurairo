94035fa9191db0cfbb864b793ebfa8422f75f2a8
cocdeshijie <2865509822ma@gmail.com>
2023-05-28 23:10:12 +0800

    add: header

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <div className={"fixed flex w-full z-50"}>
+            <div className={"w-full bg-amber-200/70 h-14 md:h-16 items-center md:rounded-2xl md:mt-5 md:mx-10"}>
+                Header
+            </div>
+        </div>
+    )
+}
