926141a64a3e17a07ba12112a60436bf882bd0fc
cocdeshijie <2865509822ma@gmail.com>
2023-05-28 23:10:28 +0800

    update: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Roboto_Mono } from 'next/font/google'
-import NavMenu from "@/app/NavMenu";
-import Footer from "@/app/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Roboto_Mono({ subsets: ['latin'] })
 
@@ -22,7 +22,7 @@ export default function RootLayout({
                 backgroundSize: 'cover',
                 backgroundAttachment: 'fixed'}}>
       <div className={"min-h-screen flex flex-col"}>
-          <NavMenu />
+          <Header />
           {children}
           <Footer />
       </div>
