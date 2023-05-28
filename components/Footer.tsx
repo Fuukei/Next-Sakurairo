8fabda744853ae77545d6e5d67050da89f16d489
cocdeshijie <2865509822ma@gmail.com>
2023-05-28 23:09:55 +0800

    update: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <div className={"mx-auto flex items-center justify-between px-8"}>
                 <div className="mt-8 md:order-1 md:mt-0">
                     <p className="text-center text-xs leading-5 text-gray-500">
-                        &copy; 2020 Your Company, Inc. All rights reserved.
+                        &copy; cocdeshijie. All rights reserved.
                     </p>
                 </div>
             </div>
