2d0329470a9702ac480aad6e49e3cf324fca810d
cocdeshijie <2865509822ma@gmail.com>
2023-05-29 23:02:32 +0800

    update: allow all hostname for image

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '*',
-                port: '',
-                pathname: '/*',
+                hostname: '**'
             }
         ]
     }
