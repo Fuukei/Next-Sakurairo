6133ada863da1b2625177c7683dcd8708031eb37
cocdeshijie <2865509822ma@gmail.com>
2023-05-30 14:21:26 +0800

    update: center elements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className={"border-t bg-white/70 dark:bg-gray-900/80 backdrop-blur-md py-8"}>
-            <div className={"mx-auto flex items-center justify-between px-8"}>
+        <footer className={"border-t flex bg-white/70 dark:bg-gray-900/80 backdrop-blur-md py-8"}>
+            <div className={"mx-auto items-center justify-between px-8"}>
                 <div className="mt-8 md:order-1 md:mt-0">
                     <p className="text-center text-xs leading-5 text-gray-500">
                         &copy; cocdeshijie. All rights reserved.
