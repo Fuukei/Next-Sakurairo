af086d59ffae9c2822fbecc2213e27b29c21053f
cocdeshijie <2865509822ma@gmail.com>
2023-05-30 23:50:26 +0800

    update: image link to article

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -8,11 +8,14 @@ export default function ArticleCard(article: Article) {
     return (
         <div className="bg-white dark:bg-gray-900/75 flex flex-col md:flex-row w-full rounded-xl overflow-hidden shadow-lg hover:shadow-2xl duration-500 mb-4 md:mb-6 md:max-h-72">
             <div className="w-full md:w-7/12 overflow-hidden">
-                <Image src={article.image}
-                       alt={"Placeholder Image"}
-                       className={"w-full h-64 md:h-full object-cover hover:scale-110 duration-500"}
-                       width={1920}
-                       height={1080}/>
+                <Link href={article.url}>
+                    <Image src={article.image}
+                           alt={"Article image"}
+                           className={"w-full h-64 md:h-full object-cover hover:scale-110 duration-500"}
+                           width={1920}
+                           height={1080}/>
+                </Link>
+
             </div>
             <div className="w-full container md:w-5/12 p-4 lg:px-8">
                 <div className={"flex mb-4"}>
