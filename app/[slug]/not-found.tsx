d12c0014a790659fa976a466edcf4f7a9edd5f74
cocdeshijie <2865509822ma@gmail.com>
2023-05-30 16:06:27 +0800

    add: basic 404 page

---
+++ app/[slug]/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <div className={"min-h-screen bg-white/60 dark:bg-gray-900/75 backdrop-blur-md"}>
+            <div className={"flex h-screen justify-center"}>
+                <div className={"self-center"}>
+                    Page not found
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -5,16 +5,15 @@ import {notFound} from "next/navigation";
 export const generateStaticParams = async () => allPages.map((page) => ({ slug: page.slug }))
 
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-    const page = allPages.find((page) => page.slug === params.slug)
-    if (!page) throw new Error(`Page not found for slug: ${params.slug}`)
+    const page = allPages.find((page) => page.slug === params.slug) || {
+        title: "Page not found"
+    }
     return { title: page.title }
 }
 
 const PageLayout = ({ params }: { params: { slug: string } }) => {
-    // Find the page for the current page.
     const page = allPages.find((page) => page.slug === params.slug)
 
-    // 404 if the page does not exist.
     if (!page) notFound()
 
     return (
