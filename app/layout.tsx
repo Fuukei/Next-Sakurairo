a047dc08a0149277b4972796f6e454e1087c5738
cocdeshijie <2865509822ma@gmail.com>
2023-06-08 03:59:21 +0800

    fix: suppress hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}
             style={{backgroundImage: 'url("https://www.loliapi.com/acg/pc/")',
                 backgroundSize: 'cover',
