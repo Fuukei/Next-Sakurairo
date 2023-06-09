d19f52e3b3b9e3da1599a96f7cd90c5866003a2f
cocdeshijie <2865509822ma@gmail.com>
2023-06-09 20:58:04 +0800

    add: separate search trigger

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -38,21 +38,6 @@ export default function Search() {
 
     return (
         <Dialog.Root open={isSearching}>
-            <Dialog.Trigger>
-                {/*<motion.div*/}
-                {/*    whileHover={{ scale: 1.2 }}*/}
-                {/*    whileTap={{ scale: 0.8 }}*/}
-                {/*    transition={{ duration: 0.3 }}*/}
-                {/*    onClick={() => toggleSearch()}*/}
-                {/*    className={cn(*/}
-                {/*        "m-2.5 inline-flex items-center justify-center rounded-md p-2.5",*/}
-                {/*        "bg-slate-50 dark:bg-slate-800 text-primary_color dark:text-primary_color-dark"*/}
-                {/*    )}*/}
-                {/*>*/}
-                {/*    <span className="sr-only">Open menu</span>*/}
-                {/*    <MagnifyingGlassIcon className={"w-6 h-6"} aria-hidden={"true"}/>*/}
-                {/*</motion.div>*/}
-            </Dialog.Trigger>
             <AnimatePresence>
                 {isSearching ? (
                     <Dialog.Portal forceMount>
