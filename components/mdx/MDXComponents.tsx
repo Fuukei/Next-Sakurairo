f6aa1c329406729cea129da291ea59bb7bf9d577
cocdeshijie <2865509822ma@gmail.com>
2023-06-09 22:05:13 +0800

    update: headings

--- components/mdx/MDXComponents.tsx
+++ components/mdx/MDXComponents.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Link from "next/link";
+import { HashtagIcon } from "@heroicons/react/24/solid";
+import { cn } from "@/lib/utils";
+import { useState } from "react"
 
 function a({ href, children }: React.HTMLProps<HTMLAnchorElement>) {
     if (href && href.startsWith('/')) {
@@ -25,32 +30,140 @@ function generateId(children: React.ReactNode): string {
 
 function H1({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h1 id={id}>{children}</h1>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h1 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                    "opacity-30 duration-200": !hover
+                },
+                    "ml-1 w-6 h-6",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h1>
+    )
 }
 
 function H2({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h2 id={id}>{children}</h2>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h2 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                        "opacity-30 duration-200": !hover
+                    },
+                    "ml-1 w-5 h-5",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h2>
+    )
 }
 
 function H3({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h3 id={id}>{children}</h3>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h3 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                        "opacity-30 duration-200": !hover
+                    },
+                    "ml-1 w-4 h-4",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h3>
+    )
 }
 
 function H4({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h4 id={id}>{children}</h4>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h4 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                        "opacity-30 duration-200": !hover
+                    },
+                    "ml-1 w-3.5 h-3.5",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h4>
+    )
 }
 
 function H5({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h5 id={id}>{children}</h5>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h5 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                        "opacity-30 duration-200": !hover
+                    },
+                    "ml-1 w-3.5 h-3.5",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h5>
+    )
 }
 
 function H6({ children }: React.HTMLProps<HTMLHeadingElement>) {
     const id = generateId(children);
-    return <h6 id={id}>{children}</h6>
+    const [hover, setHover] = useState(false);
+
+    return (
+        <h6 onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            id={id}
+            className={"flex items-baseline"}
+        >
+            {children}
+            <Link href={`#${id}`}>
+                <HashtagIcon className={cn({
+                        "opacity-30 duration-200": !hover
+                    },
+                    "ml-1 w-3.5 h-3.5",
+                    "text-primary_color dark:text-primary_color-dark"
+                )}/>
+            </Link>
+        </h6>
+    )
 }
 
 export const MDXComponents = { a, h1: H1, h2: H2, h3: H3, h4: H4, h5: H5, h6: H6 };
