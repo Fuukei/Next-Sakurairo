301a55f237c26d8192102e4c983d7c42d1ac6245
cocdeshijie <2865509822ma@gmail.com>
2023-06-13 18:00:42 +0800

    update: footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,12 @@
+import { cn } from "@/lib/utils";
+
 export default function Footer() {
     return (
-        <footer className={"border-t flex bg-slate-200/70 dark:bg-gray-900/80 backdrop-blur-md py-8"}>
+        <footer className={cn(
+            "border-t border-accent_color/50 dark:border-accent_color-dark/25",
+            "flex backdrop-blur-md py-8",
+            "bg-slate-200/70 dark:bg-gray-900/80 "
+        )}>
             <div className={"mx-auto items-center justify-between px-8"}>
                 <div className="mt-8 md:order-1 md:mt-0">
                     <p className="text-center text-xs leading-5 text-gray-500">
