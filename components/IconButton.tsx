7258a5ce4f33e34e452a42e755742d0366f2ccea
cocdeshijie <2865509822ma@gmail.com>
2023-06-16 22:19:34 +0800

    update: cursor pointer

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -11,17 +11,17 @@ type IconButtonProps = {
 
 export default function IconButton({ children, onClick }: IconButtonProps) {
     return (
-        <motion.button
+        <motion.div
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.8 }}
             transition={{ duration: 0.3 }}
             className={cn(
-                "m-2.5 inline-flex items-center justify-center rounded-md p-2.5",
+                "m-2.5 inline-flex items-center justify-center rounded-md p-2.5 cursor-pointer",
                 "bg-slate-50 dark:bg-slate-800 text-primary_color dark:text-primary_color-dark"
             )}
             onClick={onClick}
         >
             {children}
-        </motion.button>
+        </motion.div>
     )
 }
