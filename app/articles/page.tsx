1806188a314a5e69cacebde5c64e90fa563b724e
cocdeshijie <2865509822ma@gmail.com>
2023-06-19 03:10:53 +0800

    add: articles page

---
+++ app/articles/page.tsx
@@ -0,0 +1,11 @@
+export default function ArticlesPage() {
+    return (
+        <div className={"min-h-screen backdrop-blur-3xl bg-slate-50/50 dark:bg-gray-800/70"}>
+            <div className={"flex flex-col h-screen justify-center items-center"}>
+                <div className={"text-center my-8"}>
+                    Articles Page
+                </div>
+            </div>
+        </div>
+    )
+}
