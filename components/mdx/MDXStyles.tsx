fcb6271dfbe03a70f9d9f25c21d130b7e0053b85
cocdeshijie <2865509822ma@gmail.com>
2023-06-28 16:33:09 +0800

    update: improve article prose style

--- components/mdx/MDXStyles.tsx
+++ components/mdx/MDXStyles.tsx
@@ -9,12 +9,14 @@ export default function MDXStyles({ children }: MDXStylesProps) {
     return (
         <section
             className={cn(
-                "text-justify max-w-none hyphens-auto",
-                "prose prose-slate dark:prose-invert dark:text-pink-50",
-                "dark:prose-headings:text-pink-200",
-                "prose-ul:my-6 prose-li:my-0 marker:text-slate-600 dark:marker:text-slate-400",
-                "prose-a:text-accent prose-a:no-underline hover:prose-a:underline",
-                "prose-blockquote:border-l-slate-800 dark:prose-blockquote:border-l-slate-300 dark:prose-blockquote:text-pink-50",
+                "text-justify md:text-left max-w-none",
+                "prose md:prose-lg dark:prose-invert dark:text-slate-300",
+                "prose-headings:text-primary_color dark:prose-headings:text-slate-100",
+                "prose-ul:my-6 prose-li:my-0 marker:text-slate-600 dark:marker:text-slate-300",
+                "prose-a:text-primary_color-dark/80 dark:prose-a:text-primary_color/80 prose-a:no-underline",
+                "hover:prose-a:text-primary_color-dark hover:dark:prose-a:text-primary_color hover:prose-a:underline",
+                "prose-blockquote:border-l-slate-800 dark:prose-blockquote:border-l-slate-300",
+                "dark:prose-blockquote:text-primary_color",
                 "prose-hr:border-slate-700 dark:prose-hr:border-slate-300",
                 "prose-th:border-secondary_color/75 dark:prose-th:border-secondary_color-dark/75 prose-th:border-b-2 prose-th:font-bold",
                 "prose-td:border-secondary_color/50 dark:prose-td:border-secondary_color-dark/50 prose-td:border-b"
