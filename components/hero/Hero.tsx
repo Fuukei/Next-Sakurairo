186e21d79a5ee815095735e7e82d38c0661073e1
cocdeshijie <2865509822ma@gmail.com>
2023-07-06 20:53:38 +0800

    fix: type name

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import * as HeroComponents from "@/components/hero/dynamic";
 
-type LogoOptionKeys = keyof typeof HeroComponents;
+type HeroOptionKeys = keyof typeof HeroComponents;
 
-const heroOption: LogoOptionKeys = require("@/config").blogConfig.hero.option;
+const heroOption: HeroOptionKeys = require("@/config").blogConfig.hero.option;
 const SelectedHero = HeroComponents[heroOption];
 
 const Hero: FC = () => {
