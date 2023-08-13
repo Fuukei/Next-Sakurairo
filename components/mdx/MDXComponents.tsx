3e93e6d45ba744a7ae187aeebeecfb118a8fd100
cocdeshijie <2865509822ma@gmail.com>
2023-08-12 23:27:38 -0400

    update: basic image lightbox

--- components/mdx/MDXComponents.tsx
+++ components/mdx/MDXComponents.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import Link from "next/link";
+import * as Dialog from '@radix-ui/react-dialog';
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
 
 function a({ href, children }: React.HTMLProps<HTMLAnchorElement>) {
     if (href && href.startsWith('/')) {
@@ -28,7 +33,44 @@ function ul({ children, className }: React.HTMLProps<HTMLUListElement>) {
     return <ul style={style}>{children}</ul>;
 }
 
+function Img({ src, alt }: React.HTMLProps<HTMLImageElement>) {
+    const [imageOpen, setImageOpen] = useState(false)
+
+    return (
+        <Dialog.Root onOpenChange={(open) => setImageOpen(open)}>
+            <Dialog.Trigger className="flex items-center justify-center h-full mx-auto">
+                <img src={src} alt={alt} className="cursor-pointer rounded-lg"/>
+            </Dialog.Trigger>
+            <AnimatePresence>
+                {imageOpen ? (
+                    <Dialog.Portal forceMount>
+                        <motion.div
+                            onClick={() => setImageOpen(false)} // close on click
+                            initial={{ opacity: 0, scale: 0.9 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0.9 }}
+                            transition={{ ease: "easeOut", duration: 0.15 }}
+                            className={cn(
+                                "fixed z-40 inset-0 flex items-center justify-center",
+                                "bg-slate-800/50"
+                            )}
+                        >
+                            <Dialog.Content className="md:p-16">
+                                <img src={src} alt={alt} className="z-50 max-h-full max-w-full"/>
+                            </Dialog.Content>
+                        </motion.div>
+                    </Dialog.Portal>
+                ) : null}
+            </AnimatePresence>
+        </Dialog.Root>
+    );
+}
+
+const img = Img;
+
+
 export const MDXComponents = {
     a,
-    ul
+    ul,
+    img
 }
